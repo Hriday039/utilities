2c4cb812e2772afeaf3ad56321e6d757e9579353
Hriday039 <hridoyferdous@yahoo.com>
2020-07-17 01:52:30 +0600

    Updated arg_tricks.py
    - added functionality to read from a file directory or list o files

--- arg_tricks.py
+++ arg_tricks.py
@@ -4,15 +4,34 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', action='store_true')
+    # To read a specific file or list of files
+    group = parser.add_mutually_exclusive_group(required=True)
+    # To read list of files
+    group.add_argument('-f', nargs='+', help='Path of group record')
+    # To read file path
+    group.add_argument('-d', type=str, help='Directory of group record')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
+    
+    # To read list of file
+    in_file = args.f
+    # To read file from a directory
+    in_path = args.d
+    if type(in_file) == list:
+        print(in_file)
+    elif type(in_path) == str:
+        csv = '*.csv'
+        path = os.path.join(in_path, csv)
+        files = glob(path)
+        print(files)
+        
     if args.v:
         print('-v is True')
     else:
         print('-v is False')
 
 
-parser.add_argument('-v', action='store_true')
+    parser.add_argument('-v', action='store_true')
